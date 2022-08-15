4b6ca2a3a64ac5b81f388385fc05b2f9e766097a
Marguerite Choyer <margueritechoyer@gmail.com>
2022-08-15 13:18:31 +0200

    Pas beau

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   def ask
-    if params[:question]
-      @questions = @questions.select { |question| }
-    end
   end
 
   def answer
+    @answer = ""
+    if params[:question] == "I am going to work"
+      return @answer = "Great!"
+    elsif params[:question].end_with?("?")
+      return @answer = "Silly question, get dressed and go to work!"
+    elsif params[:question] == ""
+      return @answer = "Repeat your question!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
   end
 end
